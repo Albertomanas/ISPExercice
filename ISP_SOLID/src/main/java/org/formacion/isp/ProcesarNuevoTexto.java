926f1255a61eac33a04d1c2c25f626f8003feae5
Albertomanas <amanas@cifpfbmoll.eu>
2020-04-02 18:24:21 +0200

    feat(ProcesarCorreccion): Interfaz de método corrección

---
+++ ISP_SOLID/src/main/java/org/formacion/isp/ProcesarNuevoTexto.java
@@ -0,0 +1,12 @@
+package org.formacion.isp;
+
+/**
+ * Interfaz con nueva() y texto()
+ */
+public interface ProcesarNuevoTexto {
+
+    public void nueva (String palabra);
+
+    public String texto();
+
+}
